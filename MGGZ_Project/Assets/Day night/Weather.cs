5bd24a39005427118c91f0e8c77d387f17560189
Raymon-defensie <raymon_jesse2@hotmail.com>
2018-03-12 09:36:29 +0100

    Current version of the project

---
+++ MGGZ_Project/Assets/Day night/Weather.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+
+public class Weather : MonoBehaviour {
+
+    private bool isRunning = false;
+    [SerializeField]private Animator animator;
+    [SerializeField] private string triggerValue;
+
+    [SerializeField] private AudioSource source;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag=="Tracker")
+        {
+            source.Play();
+            Debug.Log("Hallo collision");
+            animator.SetTrigger(triggerValue);
+        }
+    }
+   
+}
